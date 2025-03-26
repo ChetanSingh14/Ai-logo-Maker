0da986c2d3ac5279b1a85ad67448d7db2dd57586
ChetanSingh14 <siradhanachetan14@gmail.com>
2025-03-27 02:19:38 +0530

    added useContext to access userdetail allover

--- app/generate-logo/page.jsx
+++ app/generate-logo/page.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+"use client"
+import React, { useContext } from 'react'
+import { UserDetailContex } from '../_context/UserDetailContext'
 
 function GenerateLogo() {
+  const {userDetail,setUserDetail}=useContext(UserDetailContex)
+  console.log(userDetail);
   return (
     <div>
       gekjbfds
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,11 +1,14 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Header from './_components/Header'
 import { useUser } from '@clerk/nextjs';
+import { UserDetailContex } from './_context/UserDetailContext';
 function Provider({children}) {
   const {user}=useUser();
   console.log(user);
+  const [userDetail,setUserDetail]=useState();
+
   //save user data
 
   useEffect(()=>{
@@ -18,13 +21,16 @@ function Provider({children}) {
       userEmail:user?.primaryEmailAddress?.emailAddress
     })
     console.log(result.data);
+    setUserDetail(result.data)
   }
   return (
     <div>
+      <UserDetailContex.Provider value={{userDetail,setUserDetail}}>
         <Header/>
         <div className='px-10 lg:px-32 xl:px-48 2xl:px-56'>
         {children}
         </div>
+        </UserDetailContex.Provider>
 
     </div>
   )

bc354f34a68334326b62f443d7c16c7fb5fc726d
ChetanSingh14 <siradhanachetan14@gmail.com>
2025-03-27 21:34:16 +0530

    u

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import {Tektur } from "next/font/google";
 import "./globals.css";
-import Provider from "./Provider";
+import Provider from "./provider"; 
+
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner"
 
